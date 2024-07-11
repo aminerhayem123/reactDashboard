1ba1de3682dc88b70b6c16eb1bdde02dd21a3956
amine rhayem <62341652+aminerhayem123@users.noreply.github.com>
2024-07-11 14:14:12 +0100

    delete_noimportantpages

--- react/src/routes.js
+++ react/src/routes.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'))
-const Tables = React.lazy(() => import('./views/base/tables/Tables'))
 const items = React.lazy(() => import('./views/pages/items/items'))
 const packs = React.lazy(() => import('./views/pages/packs/packs'))
 const Transactions = React.lazy(() => import('./views/pages/Transactions/Transactions'))
@@ -12,7 +11,6 @@ const Transactions = React.lazy(() => import('./views/pages/Transactions/Transac
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/dashboard', name: 'Dashboard', element: Dashboard },
-  { path: '/base/tables', name: 'Tables', element: Tables },
   { path: '/items', name: 'items', element: items },
   { path: '/packs', name: 'packs', element: packs },
   { path: '/Transactions', name: 'Transactions', element: Transactions },
