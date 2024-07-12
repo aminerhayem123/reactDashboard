04870c1cbf20b9d2faf5d03ceb57aa531192e952
amine rhayem <62341652+aminerhayem123@users.noreply.github.com>
2024-07-12 04:55:25 +0100

    browser_router

--- react/src/App.js
+++ react/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, Suspense } from 'react';
-import { HashRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'; // Change to BrowserRouter
 import { useSelector } from 'react-redux';
 import { CSpinner, useColorModes } from '@coreui/react';
 import './scss/style.scss';
@@ -10,8 +10,6 @@ const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'));
 
 // Pages
 const Login = React.lazy(() => import('./views/pages/login/Login'));
-const Page404 = React.lazy(() => import('./views/pages/page404/Page404'));
-const Page500 = React.lazy(() => import('./views/pages/page500/Page500'));
 const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard')); 
 
 const App = () => {
@@ -73,7 +71,7 @@ const App = () => {
   }
 
   return (
-    <HashRouter>
+    <BrowserRouter> {/* Change to BrowserRouter */}
       <Suspense
         fallback={
           <div className="pt-3 text-center">
@@ -89,8 +87,6 @@ const App = () => {
             name="Login Page"
             element={<Login handleLogin={handleLogin} />}
           />
-          <Route exact path="/404" name="Page 404" element={<Page404 />} />
-          <Route exact path="/500" name="Page 500" element={<Page500 />} />
           <Route
             path="*"
             name="Home"
@@ -104,7 +100,7 @@ const App = () => {
           />
         </Routes>
       </Suspense>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
